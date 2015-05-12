b404a8ebd90f1008c26c6dc4e37410b9d9ca5fb5
izzitech <id.ies.dev@gmail.com>
2015-05-12 15:51:07 -0300

    Created a new repo and added a solution.

---
+++ ParametrosGlobales.cs
@@ -0,0 +1,252 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Xml.Serialization;
+
+//--------------------------------------------------------------------------------------------------------------------------------------------------------
+//
+//      AudioRecycler
+//      Archivador de audios generados por el InnovoInformePlayer.
+//
+//      Autor: Iván E. Sierra.
+//      Fecha: 15 de Septiembre de 2014.
+//      Comentario: Esta pieza está basada en el generador de ParametrosGlobales de Innovo, pero altamente refactorizado, simplificado y un poco mejorado.
+//
+//--------------------------------------------------------------------------------------------------------------------------------------------------------
+
+namespace AudioRecycler
+{
+    public class VariablesGlobales
+    {
+        // Almacenamos la ruta del XML de ParametrosGlobales
+        public static string _rutaConParametrosGlobales = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location.ToString()) + @"\ParametrosGlobales.xml";
+        public static string UNC_Origen;
+        public static string UNC_Destino;
+        public static int diasDeGracia;
+    }
+
+    [XmlRootAttribute("ParametrosGlobales", Namespace = "", IsNullable = false)]
+    public class ParametrosGlobalesObjeto
+    {
+        // Valores de conexión a la base de datos.
+        public string serverIp { get; set; }
+        public int serverPuerto { get; set; }
+        public string baseDeDatos { get; set; }
+        public string schema { get; set; }
+        public string iam { get; set; }
+        public string verify { get; set; }
+        
+        // Ruta de la carpeta compartida que contiene los audios del InnovoInformePlayer.
+        public string UNC_Origen { get; set; }
+        // Ruta a donde se van a mover los audios.
+        public string UNC_Destino { get; set; }
+
+        // Cantidad de días de audios que se van a dejar sin archivar.
+        public int diasSinArchivar { get; set; }
+    }
+
+    public class ParametrosGlobales
+    {
+        public bool CrearParametrosGlobalesPorConsola(string salt)
+        {
+            ParametrosGlobalesObjeto parametrosGlobalesObjeto = new ParametrosGlobalesObjeto();
+
+            ConsoleColor colorOriginal = Console.ForegroundColor;
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine();
+            Design.PrintLineChar('-');
+            Console.WriteLine("Creacion del archivo ParametrosGlobales.xml");
+            Design.PrintLineChar('-');
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine();
+            Console.WriteLine("El archivo ParametrosGlobales.xml guarda la configuracion esencial del programa.");
+            Console.WriteLine("Por favor ingrese los datos que se solicitan a continuación.");
+            Console.WriteLine();
+
+            // La razón de que estén separadas las strings de consulta con la asignación de las variables,
+            // es dar la posibilidad de que tengan distinto color las preguntas de las respuestas sin tener
+            // que repetir el mismo código una y otra vez.
+            List<string> datos = new List<string>();
+            datos.Add("Direccion IP: ");
+            datos.Add("Puerto: ");
+            datos.Add("Nombre de la base de datos: ");
+            datos.Add("Schema de la base de datos: ");
+            datos.Add("Usuario: ");
+            datos.Add("Contraseña: ");
+            datos.Add("Direccion UNC donde están los audios: ");
+            datos.Add("Direccion UNC de destino: ");
+            datos.Add("Cantidad de días a mantener: ");
+
+            bool losDatosSonCorrectos = false;
+
+            while (!losDatosSonCorrectos) {
+                SolicitarDatos(ref parametrosGlobalesObjeto, datos, salt);
+                losDatosSonCorrectos = ConsultarDatos(parametrosGlobalesObjeto, datos);
+            }
+
+            TextWriter xmlStream = new StreamWriter(VariablesGlobales._rutaConParametrosGlobales);
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(ParametrosGlobalesObjeto));
+
+            xmlSerializer.Serialize(xmlStream, parametrosGlobalesObjeto);
+            xmlStream.Close();
+
+            Console.WriteLine();
+            Console.WriteLine("¡El archivo fue creado exitosamente!");
+            Console.WriteLine();
+            Console.ForegroundColor = colorOriginal;
+
+            return true;
+        }
+
+        private void SolicitarDatos(ref ParametrosGlobalesObjeto parametrosGlobalesObjeto, List<string> datosASolicitar, string salt) {
+            for (int i = 0; i < datosASolicitar.Count; i++) {
+                Console.ForegroundColor = ConsoleColor.Gray;
+                Console.Write(datosASolicitar[i]);
+                Console.ForegroundColor = ConsoleColor.White;
+
+                switch (i) {
+                    case 0:
+                        parametrosGlobalesObjeto.serverIp = Console.ReadLine();
+                        break;
+                    case 1:
+                        int puertoTemp;
+                        if (int.TryParse(Console.ReadLine(), out puertoTemp)) {
+                            parametrosGlobalesObjeto.serverPuerto = puertoTemp;
+                        } else {
+                            Console.ForegroundColor = ConsoleColor.Gray;
+                            Console.WriteLine("Los datos introducidos no pudieron ser almacenados... intente otra vez.");
+                            i--;
+                        }
+                        break;
+                    case 2:
+                        parametrosGlobalesObjeto.baseDeDatos = Console.ReadLine();
+                        break;
+                    case 3:
+                        parametrosGlobalesObjeto.schema = Console.ReadLine();
+                        break;
+                    case 4:
+                        string iamTemp = Console.ReadLine();
+                        parametrosGlobalesObjeto.iam = SSTCryptographer.Encrypt(iamTemp, parametrosGlobalesObjeto.baseDeDatos + salt);
+                        break;
+                    case 5:
+                        string verifyTemp = Console.ReadLine();
+                        parametrosGlobalesObjeto.verify = SSTCryptographer.Encrypt(verifyTemp, parametrosGlobalesObjeto.iam + salt);
+                        break;
+                    case 6:
+                        parametrosGlobalesObjeto.UNC_Origen = Console.ReadLine();
+                        break;
+                    case 7:
+                        parametrosGlobalesObjeto.UNC_Destino = Console.ReadLine();
+                        break;
+                    case 8:
+                        int diasTemp;
+                        if (int.TryParse(Console.ReadLine(), out diasTemp)) {
+                            parametrosGlobalesObjeto.diasSinArchivar = diasTemp;
+                        } else {
+                            Console.ForegroundColor = ConsoleColor.Gray;
+                            Console.WriteLine("Los datos introducidos no pudieron ser almacenados... intente otra vez.");
+                            i--;
+                        }
+                        break;
+                }
+            }
+            Console.ForegroundColor = ConsoleColor.Gray;
+        }
+
+        private bool ConsultarDatos(ParametrosGlobalesObjeto parametrosGlobalesObjeto, List<string> datosAConsultar){
+            Console.WriteLine();
+            Console.WriteLine("Verifique los datos ingresados");
+            Console.WriteLine("------------------------------");
+            for (int i = 0; i < datosAConsultar.Count; i++) {
+                Console.ForegroundColor = ConsoleColor.Gray;
+                Console.Write(datosAConsultar[i]);
+                Console.ForegroundColor = ConsoleColor.White;
+                switch (i) {
+                    case 0:
+                        Console.Write(parametrosGlobalesObjeto.serverIp + "\n");
+                        break;
+                    case 1:
+                        Console.Write(parametrosGlobalesObjeto.serverPuerto + "\n");
+                        break;
+                    case 2:
+                        Console.Write(parametrosGlobalesObjeto.baseDeDatos + "\n");
+                        break;
+                    case 3:
+                        Console.Write(parametrosGlobalesObjeto.schema + "\n");
+                        break;
+                    case 4:
+                        Console.Write("******" + "\n");
+                        break;
+                    case 5:
+                        Console.Write("******" + "\n");
+                        break;
+                    case 6:
+                        Console.Write(parametrosGlobalesObjeto.UNC_Origen + "\n");
+                        break;
+                    case 7:
+                        Console.Write(parametrosGlobalesObjeto.UNC_Destino + "\n");
+                        break;
+                    case 8:
+                        Console.Write(parametrosGlobalesObjeto.diasSinArchivar.ToString() + "\n");
+                        break;
+                }
+                Console.ForegroundColor = ConsoleColor.Gray;
+            }
+
+            while (true) {
+                Console.WriteLine();
+                Console.WriteLine("¿Son estos datos correctos? (S)í, (N)o: ");
+                ConsoleKeyInfo respuesta = Console.ReadKey(true);
+                Console.WriteLine();
+                if (respuesta.Key == ConsoleKey.S) {
+                    return true;
+                } else if (respuesta.Key == ConsoleKey.N){
+                    Console.WriteLine("Ingrese los datos nuevamente...");
+                    Console.WriteLine("-------------------------------");
+                    return false;
+                }
+            }
+        }
+        public NpgsqlConnection LeerParametrosGlobales(string SALT)
+        {
+            ParametrosGlobalesObjeto parametrosGlobalesObjeto = new ParametrosGlobalesObjeto();
+
+            NpgsqlConnection npgsqlConnection = new NpgsqlConnection();
+            StreamReader streamReader;
+
+            if (File.Exists(VariablesGlobales._rutaConParametrosGlobales))
+            {
+                try
+                {
+                    streamReader = new StreamReader(VariablesGlobales._rutaConParametrosGlobales);
+                    XmlSerializer xmlSerializer = new XmlSerializer(typeof(ParametrosGlobalesObjeto));
+                    parametrosGlobalesObjeto = (ParametrosGlobalesObjeto)xmlSerializer.Deserialize(streamReader);
+                    streamReader.Close();
+
+                    npgsqlConnection.ConnectionString =
+                                       "Server=" + parametrosGlobalesObjeto.serverIp + ";" +
+                                       "Port=" + parametrosGlobalesObjeto.serverPuerto + ";" +
+                                       "User Id=" + SSTCryptographer.Decrypt(parametrosGlobalesObjeto.iam, parametrosGlobalesObjeto.baseDeDatos + SALT) + ";" +
+                                       "Password=" + SSTCryptographer.Decrypt(parametrosGlobalesObjeto.verify, parametrosGlobalesObjeto.iam + SALT) + ";" +
+                                       "Database=" + parametrosGlobalesObjeto.baseDeDatos + ";" +
+                                       "SearchPath='" + parametrosGlobalesObjeto.schema + "';";
+
+                    VariablesGlobales.UNC_Origen = parametrosGlobalesObjeto.UNC_Origen;
+                    VariablesGlobales.UNC_Destino = parametrosGlobalesObjeto.UNC_Destino;
+                    VariablesGlobales.diasDeGracia = parametrosGlobalesObjeto.diasSinArchivar;
+
+                    return npgsqlConnection;
+                }
+                catch
+                {
+                    Console.WriteLine("Error en la lectura de los parametros globales.");
+                    Console.ReadKey();
+                }
+            }
+            return null;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,220 @@
+﻿using Npgsql;
+using NpgsqlTypes;
+using System;
+using System.Data;
+using System.IO;
+using System.Text;
+
+//-----------------------------------------------------------------------------------------------------------------------------------------------------
+//
+//      AudioRecycler
+//      Archivador de audios generados por el InnovoInformePlayer.
+//
+//      Autor: Iván E. Sierra.
+//      Fecha: 15 de Septiembre de 2014.
+//
+//-----------------------------------------------------------------------------------------------------------------------------------------------------
+
+namespace AudioRecycler {
+    public class Design {
+        public static void PrintLineChar(char character) {
+            for (int i = 0; i < Console.WindowWidth; i++) {
+                Console.Write(character);
+            }
+        }
+    }
+    class Program {
+        public static void PrintSplashScreen() {
+            ConsoleColor colorOriginal = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.White;
+
+            Design.PrintLineChar('=');
+            Console.WriteLine("AudioRecicler: archivador de audios del InnovoInformes.");
+            Design.PrintLineChar('=');
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine();
+            Console.WriteLine("Bienvenido al archivador de audios del InnovoInformes.");
+            Console.WriteLine("Este programa sirve para almacenar los audios antiguos, recuperar rendimiento y liberar espacio en disco.");
+            Console.WriteLine();
+        }
+
+        private static NpgsqlConnection CargarParametrosGlobales() {
+            ParametrosGlobales parametrosGlobales = new ParametrosGlobales();
+
+            while (!File.Exists(VariablesGlobales._rutaConParametrosGlobales)) {
+                Console.WriteLine("Intentamos crear el archivo ParametrosGlobales.xml...");
+                parametrosGlobales.CrearParametrosGlobalesPorConsola("añsldfn83892i");
+            }
+
+            Console.WriteLine("Intentamos cargar el archivo ParametrosGlobales.xml...");
+            return parametrosGlobales.LeerParametrosGlobales("añsldfn83892i");
+        }
+
+        private static DataTable ObtenerListaDeAudiosParaArchivar(NpgsqlConnection npgsqlConnection, DateTime fechaFinal) {
+            NpgsqlDataAdapter da = new NpgsqlDataAdapter("audiosobtenerparaarchivar", npgsqlConnection);
+            da.SelectCommand.CommandType = CommandType.StoredProcedure;
+
+            da.SelectCommand.Parameters.Add("_fecha", NpgsqlDbType.Timestamp);
+            da.SelectCommand.Parameters[0].Value = fechaFinal;
+
+            DataTable audiosEscuchados = new DataTable();
+            try {
+                audiosEscuchados.BeginLoadData();
+                da.Fill(audiosEscuchados);
+                audiosEscuchados.EndLoadData();
+                return audiosEscuchados;
+            } catch {
+                Console.WriteLine("Error al cargar la lista de audios desde la base de datos...");
+                return null;
+            }
+        }
+
+        private static void DeleteEmptyDirectories(string startLocation) {
+            foreach (var directory in Directory.GetDirectories(startLocation)) {
+                DeleteEmptyDirectories(directory);
+                if (Directory.GetFiles(directory).Length == 0 &&
+                    Directory.GetDirectories(directory).Length == 0) {
+                    Directory.Delete(directory, false);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Mueve los audios de una carpeta a otra, asignandoles en la base de datos el estado "ARCHIVADO".
+        /// </summary>
+        /// <param name="npgsqlConnection"></param>
+        /// <param name="dataTable"></param>
+        private static void Mover(NpgsqlConnection npgsqlConnection, DataTable dataTable) {
+            int id = 0;
+            int movidos = 0;
+            int warnings = 0;
+            StringBuilder sbErrores = new StringBuilder();
+            string rutaOrigen;
+            string archivoDestino;
+
+            for (int fila = 0; fila < dataTable.Rows.Count; fila++) {
+                id = (int)dataTable.Rows[fila]["id"];
+                rutaOrigen = (string)dataTable.Rows[fila]["ruta"];
+
+                FileInfo archivoOrigen = new FileInfo(VariablesGlobales.UNC_Origen + rutaOrigen);
+                System.Diagnostics.Debug.WriteLine("Ruta de origen del audio: " + archivoOrigen.FullName);
+
+                FileInfo rutaDestino = new FileInfo(VariablesGlobales.UNC_Destino + rutaOrigen);
+                archivoDestino = rutaDestino.DirectoryName + "\\" + id.ToString() + rutaDestino.Extension;
+                System.Diagnostics.Debug.WriteLine("Ruta de destino del audio: " + archivoDestino);
+
+                if (!Directory.Exists(archivoOrigen.DirectoryName) || !File.Exists(archivoOrigen.FullName)){
+                    System.Diagnostics.Debug.WriteLine("El archivo no existe... salteando " + archivoOrigen.FullName);
+                    continue;
+                }
+
+                    if (!Directory.Exists(rutaDestino.DirectoryName)) {
+                        System.Diagnostics.Debug.WriteLine("La ruta de destino no existe, creando " + rutaDestino);
+                        Directory.CreateDirectory(rutaDestino.DirectoryName);
+                    }
+
+                    File.Copy(archivoOrigen.FullName, archivoDestino);
+                if (AudioArchivarEnDB(npgsqlConnection, id)){
+                        File.Delete(archivoOrigen.FullName);
+                    movidos += 1;
+                    Console.WriteLine();
+                    Console.WriteLine("Movido: " + archivoOrigen.FullName);
+                    Console.WriteLine("A:      " + archivoDestino);
+                }
+            }
+
+            DeleteEmptyDirectories(VariablesGlobales.UNC_Origen);
+
+            Console.WriteLine();
+            Design.PrintLineChar('-');
+            Console.WriteLine("Se han almacenado {0} audios.", movidos);
+            Design.PrintLineChar('-');
+            Console.WriteLine();
+            if (warnings > 0){
+                Console.WriteLine("Han ocurrido {0} errores.", warnings.ToString());
+            }
+            Console.WriteLine();
+        }
+
+        private static bool AudioArchivarEnDB(NpgsqlConnection npgsqlConnection, int idAudioAArchivar) {
+            NpgsqlCommand cmd = new NpgsqlCommand("audioarchivar", npgsqlConnection);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            cmd.Parameters.Add("_audioid", NpgsqlTypes.NpgsqlDbType.Integer);
+            cmd.Parameters[0].Value = idAudioAArchivar;
+
+                cmd.Connection.Open();
+                cmd.ExecuteNonQuery();
+                cmd.Connection.Close();
+                return true;
+        }
+
+        private static void MostrarAyuda() {
+            Design.PrintLineChar('-');
+            Console.WriteLine("Los parametros que se pueden utilizar en este programa son los siguientes");
+            Design.PrintLineChar('-');
+            Console.WriteLine("-h             Este mensaje de ayuda.");
+            Console.WriteLine("-cXXX          'XXX' = cantidad de columnas de la ventana de la consola.");
+            Console.WriteLine("-rYYY          'YYY' = cantidad de filas de la ventana de la consola.");
+            Console.WriteLine("-a             Cierra la aplicación automáticamente cuando concluye.");            
+            Design.PrintLineChar('-');
+        }
+
+        static void Main(string[] args) {
+            int columns = 120;
+            int rows = 50;
+            bool autoClose = false;
+
+            NpgsqlConnection npgsqlConnection;
+            DataTable listaDeAudios;
+
+            // Revisamos los parámetros usados al llamar la aplicación
+            for (int i = 0; i < args.Length; i++) {
+                if (args[i].Length < 2) {
+                    continue;
+                }
+                switch (args[i].Substring(0,2)) {
+                    case "-h":
+                        PrintSplashScreen();
+                        MostrarAyuda();
+                        Console.ReadKey();
+                        return;
+                    case "-c":
+                        int columnsTemp;
+                        int.TryParse(args[i].Substring(2), out columnsTemp);
+                        columns = columnsTemp;
+                        break;
+                    case "-r":
+                        int rowsTemp;
+                        int.TryParse(args[i].Substring(2), out rowsTemp);
+                        rows = rowsTemp;
+                        break;
+                    case "-a":
+                        autoClose = true;
+                        break;
+                }
+            }
+
+            Console.SetWindowSize(columns, rows);
+            PrintSplashScreen();
+
+            npgsqlConnection = CargarParametrosGlobales();
+
+            // Restamos al día de hoy la cantidad de días de gracia para saber a partir de que fecha empezar a borrar hacia el pasado.
+            DateTime fechaFinal = DateTime.Today.AddDays(-VariablesGlobales.diasDeGracia);
+            Console.WriteLine("Obteniendo audios escuchados de hasta hace {0} días, fecha final: {1}", VariablesGlobales.diasDeGracia, fechaFinal.ToString());
+            listaDeAudios = ObtenerListaDeAudiosParaArchivar(npgsqlConnection, fechaFinal);
+
+            // Movemos los audios de una carpeta a otra, reportando en la base de datos que el archivo fue "ARCHIVADO".
+            if (listaDeAudios != null) {
+                Mover(npgsqlConnection, listaDeAudios);
+            } else {
+                Console.WriteLine("La base de datos no ha devuelto resultados.");
+            }
+
+            if (!autoClose) {
+                Console.ReadKey();
+            }
+        }
+    }
+}
